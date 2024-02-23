6192a3c55e674082c8f7d4d2eefce4b26cbcadcb
Kyle Mak <kjmmak@gmail.com>
2024-02-22 23:26:30 -0800

    Time: 68 ms (83.18%) | Memory: 59.2 MB (23.87%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let buy = 0;
+    let sell = 1;
+    let max_profit = 0;
+    while (sell < prices.length) {
+        if (prices[buy] < prices[sell]) {
+            let profit = prices[sell] - prices[buy];
+            max_profit = Math.max(max_profit, profit);
+        } else {
+            buy = sell;
+        }
+        sell++;
+    }
+    return max_profit;
+};
